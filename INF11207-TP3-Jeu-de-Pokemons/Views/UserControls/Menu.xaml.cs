a3d2109c49ba9e2b0d834190d999904b7ac9e3ac
Bastien Goulet <bastien.goulet27@outlook.com>
2021-03-26 20:14:55 -0500

    Création de l'interface Statistiques et du UserControl JaugeXP

--- INF11207-TP3-Jeu-de-Pokemons/Views/UserControls/Menu.xaml.cs
+++ INF11207-TP3-Jeu-de-Pokemons/Views/UserControls/Menu.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Windows.Controls;
 
-namespace INF11207_TP3_Jeu_de_Pokemons.Views
+namespace INF11207_TP3_Jeu_de_Pokemons.Views.UserControls
 {
     /// <summary>
     /// Interaction logic for Menu.xaml
