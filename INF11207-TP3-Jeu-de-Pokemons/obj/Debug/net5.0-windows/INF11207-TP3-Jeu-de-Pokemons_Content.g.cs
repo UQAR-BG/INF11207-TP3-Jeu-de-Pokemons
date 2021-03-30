96774d3bdda8b4f8df428ab42f32cff8f5e16dec
Bastien Goulet <bastien.goulet27@outlook.com>
2021-03-30 09:15:11 -0500

    Ajout des images de pokémons

--- INF11207-TP3-Jeu-de-Pokemons/obj/Debug/net5.0-windows/INF11207-TP3-Jeu-de-Pokemons_Content.g.cs
+++ INF11207-TP3-Jeu-de-Pokemons/obj/Debug/net5.0-windows/INF11207-TP3-Jeu-de-Pokemons_Content.g.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/images/pokemon.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/images/1.png")]
 
 
