36d0f3f46c6fdab9cbc3dc29e5cbb3dfb481f4c9
Bastien Goulet <bastien.goulet27@outlook.com>
2021-04-09 13:00:25 -0500

    Création du modèle Invitation

---
+++ INF11207-TP3-Jeu-de-Pokemons/Models/Combats/Invitation.cs
@@ -0,0 +1,51 @@
+﻿using INF11207_TP3_Jeu_de_Pokemons.Enums;
+using Newtonsoft.Json;
+
+namespace INF11207_TP3_Jeu_de_Pokemons.Models.Combats
+{
+    public class Invitation
+    {
+        private StatutType statut;
+        private string nomDestinataire;
+        private string nomCreateur;
+
+        [JsonIgnore]
+        private Dresseur createur;
+
+        public StatutType Statut
+        {
+            get { return statut; }
+        }
+
+        public string NomDestinataire
+        {
+            get { return nomDestinataire; }
+        }
+
+        public string NomCreateur
+        {
+            get { return nomCreateur; }
+        }
+
+        [JsonConstructor]
+        public Invitation() { }
+
+        public Invitation(Dresseur createur)
+        {
+            statut = StatutType.Attente;
+
+            this.createur = createur;
+            nomCreateur = createur.Name;
+        }
+
+        public void Confirmer()
+        {
+
+        }
+
+        public void Refuser()
+        {
+
+        }
+    }
+}
