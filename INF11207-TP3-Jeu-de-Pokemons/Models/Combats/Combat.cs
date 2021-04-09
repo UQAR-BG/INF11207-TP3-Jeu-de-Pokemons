d867114fe50fbdc77ac3d44f423ce9d23354b32a
Bastien Goulet <bastien.goulet27@outlook.com>
2021-04-09 11:33:23 -0500

    Création du modèle Combat

---
+++ INF11207-TP3-Jeu-de-Pokemons/Models/Combats/Combat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF11207_TP3_Jeu_de_Pokemons.Models.Combats
+{
+    public class Combat
+    {
+        private int mise;
+        private Dresseur joueur;
+        private Dresseur adversaire;
+
+        public int Mise
+        {
+            get { return mise; }
+        }
+
+        public Combat(Dresseur joueur, Dresseur adversaire, int mise)
+        {
+            this.joueur = joueur;
+            this.adversaire = adversaire;
+            this.mise = mise;
+        }
+
+        public void MettreFin()
+        {
+
+        }
+
+        private void DeterminerGagnant()
+        {
+
+        }
+
+        private void RemporterMise(Dresseur dresseur)
+        {
+
+        }
+
+        private void AjouterExperience()
+        {
+
+        }
+    }
+}
